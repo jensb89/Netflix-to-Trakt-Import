0705b7ea1b22b6b65779957eae1c07c77dfba0f3
Jens Brauer <jensb89@users.noreply.github.com>
2021-12-20 14:13:03 +0100

    Introduces config option to change datetime format
    Fixes #4

--- config.py
+++ config.py
@@ -4,6 +4,10 @@ LOG_FILENAME = 'Netflix2TraktImportLog.log'
 LOG_LEVEL=logging.INFO
 VIEWING_HISTORY_FILENAME = "NetflixViewingHistory.csv"
 
+# Set the datetime format of the csv file (default: %d.%m.%y for 05.02.21)
+# Use %Y-%m-%d for 2021-02-05 (Canada, ...)
+CSV_DATETIME_FORMAT = "%d.%m.%y"
+
 TMDB_API_KEY = ""
 TMDB_LANGUAGE = "en"
 TMDB_DEBUG = False
