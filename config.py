29ed43ea07f0caa38328b0368db4803dd5dd3dd9
Jens Brauer <jensb89@users.noreply.github.com>
2023-07-11 20:21:18 +0200

    Updated the config with more descirption for the datetime format
    Relates to #39

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ VIEWING_HISTORY_FILENAME = "NetflixViewingHistory.csv"
 
 # Set the datetime format of the csv file (default: %d.%m.%y for 05.02.21)
 # Use %Y-%m-%d for 2021-02-05 (Canada, ...)
+# For the format 17.05.2023 use the datetime format %d.%m.%Y (note the capital Y for 2023 instead of y for 23)
 CSV_DATETIME_FORMAT = "%d.%m.%y"
 
 TMDB_API_KEY = ""
